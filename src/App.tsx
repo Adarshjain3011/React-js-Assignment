37247b0792df5d881918d237d4895dea8e6930a7
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-06-25 14:21:25 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Homepage from './Homepage';
+import SingleCardPage from './SingleCardPage';
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/singlecartPage/:id' element={<SingleCardPage />} />
+
+        <Route path='/' element={<Homepage />} />
+
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
+
+
